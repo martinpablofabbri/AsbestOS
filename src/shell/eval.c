c41257edf76368a849d9aa7fa36ad9808892935b
keeganryan <karyan@caltech.edu>
2016-01-11 01:28:22 -0800

    Piping between commands works

--- src/shell/eval.c
+++ src/shell/eval.c
@@ -9,16 +9,26 @@
 #include "command.h"
 
 /**
- * Fill in the file descriptors of the command
- * execution structure
+ * Return a new file descriptor that should
+ * be used as input to the first command.
  *
- * Returns true on success, false otherwise.
+ * Returns -1 on error
  */
-bool set_stream_fds (cmd_exec_info *c) {
-  c->input = STDIN_FILENO;
-  c->output = STDOUT_FILENO;
-  c->error = STDERR_FILENO;
-  return true;
+int setup_first_pipe (Command *c) {
+  // TODO(keegan): Make this one end of a pipe istead of stdin
+  // TODO(keegan): handle error case in dup
+  return dup(STDIN_FILENO);
+}
+
+/**
+ * Tie the final pipe to either stdout or where specified
+ *
+ * Returns the file descriptor of the final stream
+ * on success, -1 on failure.
+ */
+int setup_last_pipe (Command *c) {
+  // TODO(keegan): Handle dup error case
+  return dup(STDOUT_FILENO);
 }
 
 /**
@@ -32,10 +42,26 @@ void start_execution (cmd_exec_info *c) {
     // TODO(keegan): Print out detailed error msg
   } else if (p) {
     // Parent
-    printf("The parent is executing!\n");
     c->pid = p;
+    // Close the file descriptors we don't need any more
+    if (close(c->input) != 0) {
+      // TODO(keegan): handle error
+    }
+    if (close(c->output) != 0) {
+      // TODO(keegan): handle error
+    }
+    if (close(c->error) != 0) {
+      // TODO(keegan): handle error
+    }
   } else {
     // Child
+    // Tie together input, output, and stderr
+    // TODO(keegan): handle error case
+    dup2(c->input, STDIN_FILENO);
+    dup2(c->output, STDOUT_FILENO);
+    dup2(c->error, STDERR_FILENO);
+    // TODO(keegan): remove the file descriptors we don't need
+
     // execvp the specified function
     char **argv = c->cmd->argv;
     execvp(argv[0], argv);
@@ -45,25 +71,47 @@ void start_execution (cmd_exec_info *c) {
 
 /** Evaluate all commands in Command_vec **/
 void eval(Command_vec cv) {
-  /* Basic steps to evaluate:
-       Loop over the cv datastructure
-         Get necessary file descriptors
-	 fork
-           child - replace stdin and stdout with file descriptors
-	   parent - do any glue necessary
-	          - wait for child
-  */
   unsigned i;
+  /* All commands are connected with pipes.
+     The first command is hooked up to either
+     STDIN or something as specified in the command.
+     The last command is hooked up to either
+     STDOUT or something as specified in the command.
+  */
+  if (cv.size == 0) {
+    return;
+  }
+
+  int input_pipe_fd = setup_first_pipe (&cv.commands[0]);
+  int bridge_pipe_fd[2];
+  // TODO(keegan): handle errors here
   for (i=0; i < cv.size; i++) {
-    printf("Eval %d\n", i);
     Command *command = &cv.commands[i];
     cmd_exec_info c;
     c.cmd = command;
-    
-    set_stream_fds(&c);
+    c.input = input_pipe_fd;
+    if (i < cv.size - 1) {
+      // Make pipe that will bridge between commands
+      if (pipe(bridge_pipe_fd) != 0) {
+	// TODO(keegan): handle error
+      }
+      c.output = bridge_pipe_fd[1];
+      input_pipe_fd = bridge_pipe_fd[0];
+    } else {
+      // This is the last command
+      int final_stream_fd = setup_last_pipe(command);
+      // TODO(keegan): handle error
+      c.output = final_stream_fd;
+    }
+    c.error = dup(STDERR_FILENO);
+    // TODO(keegan): handle error
+
     start_execution(&c);
     // TODO(keegan): Add error handling for start_command
-    // and wait()
+  }
+
+  for (i=0; i < cv.size; i++) {
+    // TODO(keegan): Add error handling
     wait(NULL);
   }
 }
@@ -73,17 +121,26 @@ void eval_tests () {
 
   // Set up a command vector
   Command_vec cv;
-  Command c;
-  c.argc = 1;
-  char **argv = (char **)malloc(sizeof(char*)*(c.argc + 1));
+  Command c[2];
+  c[0].argc = 2;
+  char **argv = (char **)malloc(sizeof(char*)*(c[0].argc + 1));
   argv[0] = "ls";
-  argv[1] = NULL;
-  c.argv = argv;
-  c.input = c.output = c.error = NULL;
-  c.is_builtin = false;
+  argv[1] = "-a";
+  argv[2] = NULL;
+  c[0].argv = argv;
+  c[0].input = c[0].output = c[0].error = NULL;
+  c[0].is_builtin = false;
 
-  cv.size = 1;
-  cv.commands = &c;
+  c[1].argc = 1;
+  argv = (char **)malloc(sizeof(char*)*(c[1].argc + 1));
+  argv[0] = "sort";
+  argv[1] = NULL;
+  c[1].argv = argv;
+  c[1].input = c[1].output = c[1].error = NULL;
+  c[1].is_builtin = false;
+
+  cv.size = 2;
+  cv.commands = c;
 
   eval(cv);
 }
