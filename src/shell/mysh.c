ca7315774b67dfe77b5dd1a3d92c3ca889ace825
Jalen Green <jpgreen@caltech.edu>
2016-01-13 00:08:02 -0800

    Add command prompt

--- src/shell/mysh.c
+++ src/shell/mysh.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 #include "read.h"
 #include "eval.h"
 
 int main(int argc, char *argv[])
 {
-  // TODO(jg): remove
-  printf("Entered main in mysh.c\n");
-
   for (;;) {
+    char *cwd = getcwd(NULL, 0);
+    printf("%s:%s>", getlogin(), cwd);
     eval(read_commands());
+
+    free(cwd);
     // TODO(jg): remove when we can input and execute an exit command
     break;
   }
-
+  printf("\n");
   return 0;
 }
