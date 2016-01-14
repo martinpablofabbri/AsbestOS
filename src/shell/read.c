7767869edc87578bc7d3617056a1babb051e5a43
keeganryan <karyan@caltech.edu>
2016-01-14 00:31:57 -0800

    Restructure, working now

--- src/shell/read.c
+++ src/shell/read.c
@@ -24,28 +24,34 @@ Command_vec* read_commands(char* string) {
   int sizeWord = 0;
   char* tmpBuffer;
   int i;
-  for (i = 0; i <= strlen(string); i++)
+  while (1)
   {
-    if (string[i] == ' ' || string[i] == '\0')
-    {
-      // sizeWord++;
-      tmpBuffer = (char*) malloc(sizeof(char) * (i - sizeWord) + 1);
-      memcpy(tmpBuffer, &string[sizeWord], (i - sizeWord));
-      tmpBuffer[i - sizeWord] = '\0';
-      sizeWord = i;
-      // sizeWord is the idx of first character of the next token
-      while (string[sizeWord] == ' ') {sizeWord++;i++;}
-      numWords++;
-      cmd->argv = (char**) realloc(cmd->argv, sizeof(char*) * (numWords + 1));
-      cmd->argv[numWords - 1] = tmpBuffer;
+
+    // Read in word
+    while (string[i] != ' ' && string[i] != '\0' && string[i] != '|') {
+      i++;
     }
 
-    // If we're at the end, we also want to finalize command structure.
-    if (string[i] == '|' || string[i] == '\0')
-    {
-      cmd->argc = numWords;
-      cmd->argv[numWords] = NULL;
-      if (strcmp(cmd->argv[0], "cd") == 0)
+    // End of word here
+    tmpBuffer = (char*) malloc(sizeof(char) * (i - sizeWord) + 1);
+    memcpy(tmpBuffer, &string[sizeWord], (i - sizeWord));
+    tmpBuffer[i - sizeWord] = '\0';
+    sizeWord = i;
+    numWords++;
+    cmd->argv = (char**) realloc(cmd->argv, sizeof(char*) * (numWords + 1));
+    cmd->argv[numWords - 1] = tmpBuffer;
+    cmd->argc = numWords;
+    cmd->argv[numWords] = NULL;
+
+    // sizeWord is the idx of first character of the next token
+    // chomp whitespace
+
+    // If there's more to the command
+    while (string[sizeWord] == ' ') {sizeWord++;i++;}
+
+    // If we need another command
+    if (string[i] == '|') {
+      /*      if (strcmp(cmd->argv[0], "cd") == 0)
       {
         cmd->is_builtin = true;
       }
@@ -56,20 +62,21 @@ Command_vec* read_commands(char* string) {
       else if (strcmp(cmd->argv[0], "exit") == 0)
       {
         cmd->is_builtin = true;
-      }
+	}*/
       nextCV->command = cmd;
-      if (string[i] != '\0') {
-	cmd = (Command *) malloc(sizeof(Command));
-	Command_vec *cv = (Command_vec *) malloc(sizeof(Command_vec*));
-	nextCV->next = cv;
-	cmd->input = NULL;
-	cmd->output = NULL;
-	cmd->error = NULL;
-      } else {
-	nextCV->next = NULL;
-      }
+      cmd = (Command *) malloc(sizeof(Command));
+      Command_vec *cv = (Command_vec *) malloc(sizeof(Command_vec*));
+      nextCV->next = cv;
+      cmd->input = NULL;
+      cmd->output = NULL;
+      cmd->error = NULL;
       nextCV = nextCV->next;
+    }
 
+    if (string[i] == '\0') {
+      nextCV->command = cmd;
+      nextCV->next = NULL;
+      break;
     }
   }
   return cv;
