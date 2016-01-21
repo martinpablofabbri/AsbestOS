f5b4967601c433f2d14d533ff395e4badbed0222
Martin Fabbri <martinpablofabbri@gmail.com>
2016-01-20 22:01:47 -0800

    Changed game struct usage to reflect branch:feature/game_data Deleted now irrelevant notes on game struct Added catching/missing balls with paddle

--- src/booter/game.c
+++ src/booter/game.c
@@ -15,13 +15,20 @@ void c_start(void) {
   }
 }
 
-void update_game(Game *) {
-  // TODO: Update game struct
-  Game->timer++;
+void update_game(game_state game) {
 
-  if (Game->timer % 20 == 0) {
+/* TODO: - UPDATE DIFFICULTY & NEW BALLS WITH TIME
+  game.timer++;
+
+  if (game.timer % 500 == 0) {
     // visible time step
+    game.difficulty++;
   }
+
+  if (game.timer % 20 == 0) {
+    // TODO: Add new ball
+  }
+*/
 
   /*********************************
    * UPDATE PADDLES ON KEY PRESSES *
@@ -29,44 +36,44 @@ void update_game(Game *) {
   // Check to make sure paddle stays on screen
   // 20 horizontal paddle positions
   if (is_pressed(A_KEY))     {
-    if (Game->left->paddlePos > 0.5) {
-      Game->left->paddlePos -= 0.05;
+    if (game.left.paddle_pos > 0.05) {
+      game.left.paddle_pos -= 0.05;
     }
   }
 
   else if (is_pressed(D_KEY)) {
-    if (Game->left->paddlePos < 0.95) {
-      Game->left->paddlePos += 0.05;
+    if (game.left.paddle_pos < 0.95) {
+      game.left.paddle_pos += 0.05;
     }
   }
 
   if (is_pressed(J_KEY))     {
-    if (Game->right->paddlePos > 0.5) {
-      Game->right->paddlePos -= 0.05;
+    if (game.right.paddle_pos > 0.5) {
+      game.right.paddle_pos -= 0.05;
     }
   }
 
   else if (is_pressed(L_KEY)) {
-    if (Game->right->paddlePos < 0.95) {
-      Game->right->paddlePos += 0.05;
+    if (game.right.paddle_pos < 0.95) {
+      game.right.paddle_pos += 0.05;
     }
   }
 
   /**********************************
    * UPDATE BALLS FALLING WITH TIME *
    **********************************/
-  Balls* currLeftBalls = Game->left->balls;
+  ball_list* currLeftBalls = game.left.balls;
   // Move Balls down a little
   while (currLeftBalls != NULL)  {
-    currLeftBalls->y += 0.01 * Game->difficulty;
+    currLeftBalls->ball.y += 0.01 * game.difficulty;
     // Go to next Ball
     currLeftBalls = currLeftBalls->next;
   }
 
-  Balls* currRightBalls = Game->right->balls;
+  ball_list* currRightBalls->next = game.right.balls;
   // Move Balls down a little
   while (currRightBalls != NULL)  {
-    currRightBalls->y += 0.01 * Game->difficulty;
+    currRightBalls->ball.y += 0.01 * game.difficulty;
     // Go to next Ball
     currRightBalls = currRightBalls->next;
   }
@@ -74,6 +81,48 @@ void update_game(Game *) {
   /************************
    * CHECK IF CAUGHT BALL *
    ************************/
+  ball_list* lastLeftBall = game.left.balls->prev;
+  if (lastLeftBall->ball->y < 1) {
+    if (game.left.paddle_pos + 0.05 > lastLeftBall->ball.x) {
+      if (game.left.paddle_pos - 0.05 < lastLeftBall->ball.x) {
+        // paddle hit ball
+        game.left.score++;
+        lastLeftBall->prev == NULL;
+        // TODO: free lastLeftBall;
+      } else {
+        // paddle didn't hit ball
+        game.left.health--;
+        lastLeftBall->prev == NULL;
+        // TODO: free lastLeftBall;
+      }
+    } else {
+      // paddle didn't hit ball
+      game.left.health--;
+      lastLeftBall->prev == NULL;
+      // TODO: free lastLeftBall;
+    }
+  }
 
+  ball_list* lastRightBall = game.right.balls->prev;
+  if (lastRightBall->ball->y < 1) {
+    if (game.right.paddle_pos + 0.05 > lastRightBall->ball.x) {
+      if (game.right.paddle_pos - 0.05 < lastRightBall->ball.x) {
+        // paddle hit ball
+        game.right.score++;
+        lastRightBall->prev == NULL;
+        // TODO: free lastRightBall;
+      } else {
+        // paddle didn't hit ball
+        game.right.health--;
+        lastRightBall->prev == NULL;
+        // TODO: free lastRightBall;
+      }
+    } else {
+      // paddle didn't hit ball
+      game.right.health--;
+      lastRightBall->prev == NULL;
+      // TODO: free lastRightBall;
+    }
+  }
 
 }
