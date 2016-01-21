1ad9833d8a1b99ec1d92a0e6138ea1b891a1a29a
Martin Fabbri <martinpablofabbri@gmail.com>
2016-01-20 21:28:22 -0800

    Move Paddles left/right; falling balls

--- src/booter/game.c
+++ src/booter/game.c
@@ -17,4 +17,63 @@ void c_start(void) {
 
 void update_game(Game *) {
   // TODO: Update game struct
+  Game->timer++;
+
+  if (Game->timer % 20 == 0) {
+    // visible time step
+  }
+
+  /*********************************
+   * UPDATE PADDLES ON KEY PRESSES *
+   *********************************/
+  // Check to make sure paddle stays on screen
+  // 20 horizontal paddle positions
+  if (is_pressed(A_KEY))     {
+    if (Game->left->paddlePos > 0.5) {
+      Game->left->paddlePos -= 0.05;
+    }
+  }
+
+  else if (is_pressed(D_KEY)) {
+    if (Game->left->paddlePos < 0.95) {
+      Game->left->paddlePos += 0.05;
+    }
+  }
+
+  if (is_pressed(J_KEY))     {
+    if (Game->right->paddlePos > 0.5) {
+      Game->right->paddlePos -= 0.05;
+    }
+  }
+
+  else if (is_pressed(L_KEY)) {
+    if (Game->right->paddlePos < 0.95) {
+      Game->right->paddlePos += 0.05;
+    }
+  }
+
+  /**********************************
+   * UPDATE BALLS FALLING WITH TIME *
+   **********************************/
+  Balls* currLeftBalls = Game->left->balls;
+  // Move Balls down a little
+  while (currLeftBalls != NULL)  {
+    currLeftBalls->y += 0.01 * Game->difficulty;
+    // Go to next Ball
+    currLeftBalls = currLeftBalls->next;
+  }
+
+  Balls* currRightBalls = Game->right->balls;
+  // Move Balls down a little
+  while (currRightBalls != NULL)  {
+    currRightBalls->y += 0.01 * Game->difficulty;
+    // Go to next Ball
+    currRightBalls = currRightBalls->next;
+  }
+
+  /************************
+   * CHECK IF CAUGHT BALL *
+   ************************/
+
+
 }
