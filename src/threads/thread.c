ca912b5cb44cb691bf9906ad091910ccc68c0dfc
Keegan Ryan <karyan@caltech.edu>
2016-01-31 16:39:30 -0800

    Add base priority field

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -425,6 +425,7 @@ static void init_thread(struct thread *t, const char *name, int priority) {
     t->status = THREAD_BLOCKED;
     strlcpy(t->name, name, sizeof t->name);
     t->stack = (uint8_t *) t + PGSIZE;
+    t->base_priority = priority;
     t->priority = priority;
     t->magic = THREAD_MAGIC;
 
