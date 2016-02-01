5b0e88f87df10b943e9ad554726d17dc8f742cb3
Keegan Ryan <karyan@caltech.edu>
2016-01-31 16:39:30 -0800

    Add base priority field

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -99,6 +99,7 @@ struct thread {
                                            runs at. */
     int base_priority;                  /*!< Priority which the
 					  process assigns to itself. */
+<<<<<<< HEAD
     struct list_elem allelem;           /*!< List element for all
 					  threads list. */
     struct list donors;                 /*!< List of all threads which
@@ -106,6 +107,9 @@ struct thread {
                                            this thread. */
     struct thread* donee;               /*!< Pointer to the thread
 					  receiving a donation. */
+=======
+    struct list_elem allelem;           /*!< List element for all threads list. */
+>>>>>>> Add base priority field
     /**@}*/
 
     /*! Shared between thread.c and synch.c. */
