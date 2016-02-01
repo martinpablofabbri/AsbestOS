8febac3dea0a21aea245926305042879779e7abf
Keegan Ryan <karyan@caltech.edu>
2016-01-31 16:59:50 -0800

    Add to and remove from donors list when appropriate

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -160,6 +160,8 @@ void add_to_ready_queue(struct thread *t);
 int thread_get_priority(void);
 void thread_set_priority(int);
 
+void thread_donate_priority(struct thread* t);
+
 int thread_get_nice(void);
 void thread_set_nice(int);
 int thread_get_recent_cpu(void);
