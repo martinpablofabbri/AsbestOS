24e29931f780c9171c08ff2f87f9058d6826e392
Keegan Ryan <karyan@caltech.edu>
2016-01-31 17:12:10 -0800

    Handle recursively updating recursive donations

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -229,18 +229,11 @@ bool lock_try_acquire(struct lock *lock) {
     make sense to try to release a lock within an interrupt
     handler. */
 void lock_release(struct lock *lock) {
-    //TODO(keegan): Do we have to disable interrupts here?
-    enum intr_level old_level;
-
     ASSERT(lock != NULL);
     ASSERT(lock_held_by_current_thread(lock));
 
-    old_level = intr_disable();
-
     lock->holder = NULL;
     sema_up(&lock->semaphore);
-
-    intr_set_level(old_level);
 }
 
 /*! Returns true if the current thread holds LOCK, false
