5663e93234ceedd5039f59ed86ef13570acfdf96
Keegan Ryan <karyan@caltech.edu>
2016-02-03 18:48:51 -0800

    Added files useful for fixed point implementation

---
+++ src/threads/fixed_point.c
@@ -0,0 +1,1 @@
+#include "threads/fixed_point.h"
---
+++ src/threads/fixed_point.h
@@ -0,0 +1,15 @@
+/*! \file fixed_point.h
+ *
+ * Declarations for the fixed point arithmetic implementation
+ */
+
+#ifndef THREADS_FIXED_POINT_H
+#define THREADS_FIXED_POINT_H
+
+#include <stdint.h>
+
+/*! Fixed point float type. */
+typedef uint32_t float_fp;
+
+#endif /* threads/fixed_point.h */
+
