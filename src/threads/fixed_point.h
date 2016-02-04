ab1962156fc236364a06853252612fe2e8674875
Jalen Green <jpgreen@caltech.edu>
2016-02-03 20:20:11 -0800

    Add fixed-point operations

--- src/threads/fixed_point.h
+++ src/threads/fixed_point.h
@@ -9,7 +9,22 @@
 #include <stdint.h>
 
 /*! Fixed point float type. */
-typedef uint32_t float_fp;
+typedef int32_t fixed;
+
+/*! Conversions between fixed and int */
+fixed int2fixed(int32_t n);
+int32_t fixed2intRoundTowardZero(fixed x);
+int32_t fixed2intRoundClosest(fixed x);
+
+/*! Fixed-point operators */
+fixed fixedAdd(fixed lhs, fixed rhs);
+fixed fixedSubtract(fixed lhs, fixed rhs);
+fixed fixedAddInt(fixed lhs, int32_t rhs);
+fixed fixedSubtractInt(fixed lhs, int32_t rhs);
+fixed fixedMultiply(fixed lhs, fixed rhs);
+fixed fixedMultiplyInt(fixed lhs, int32_t rhs);
+fixed fixedDivide(fixed lhs, fixed rhs);
+fixed fixedDivideInt(fixed lhs, int32_t rhs);
 
 #endif /* threads/fixed_point.h */
 
